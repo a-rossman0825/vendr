9420d77fbab8531329480c63aed2ee916ead505c
Anthony Rossman <anthony.rossman0825@gmail.com>
2025-06-03 15:28:30 -0600

    Style cards

--- app/models/item.js
+++ app/models/item.js
@@ -13,7 +13,7 @@ export class Item {
 
   get itemCardHTMLTemplate() {
     return `
-      <div class="col-3 mt-4">
+      <div class="col-md-3 mt-4">
         <div class="rounded text-dark">
           <div class="row justify-content-center">
             <img
@@ -21,16 +21,16 @@ export class Item {
               class="rounded col-12 card-image"
               alt="bag of chips"/>
           </div>
-          <div class="row">
-            <div class="col-8 text-light ms-1 mt-2">
-              <div class="row">
-                <small class="col-12 text-start">${this.name}</small>
+          <div class="row justify-content-center justify-content-lg-around">
+            <div class="col-4 col-md-8 text-light ms-1 mt-2">
+              <div class="row text-start">
+                <small class="col-12">${this.name}</small>
               </div>
               <div class="row">
-                <small class="col-12">$${this.price}</small>
+                <small class="col-12 ">$${this.price}</small>
               </div>
             </div>
-            <button class="col-3 mt-3 rounded bg-success text-light"><small>Buy</small></button>
+            <button class="col-2 col-md-3 mt-3 rounded bg-success text-light"><small>Buy</small></button>
           </div>
         </div>
       </div>
